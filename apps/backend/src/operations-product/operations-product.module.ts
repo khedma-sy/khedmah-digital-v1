80558173c2da54fd71b379460704948e3905eabf
khedma-sy <khedmasy@gmail.com>
2026-09-02 04:25:39 +0400

    feat: wire analytics into operations product

--- apps/backend/src/operations-product/operations-product.module.ts
+++ apps/backend/src/operations-product/operations-product.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { IdentityModule } from '../identity/identity.module';
+import { AnalyticsModule } from '../analytics/analytics.module';
 import { OperationsProductController } from './operations-product.controller';
 import { OperationsProductRepository } from './operations-product.repository';
 import { OperationsRbacService } from './operations-rbac.service';
 import { OperationsProductService } from './operations-product.service';
-@Module({ imports: [IdentityModule], controllers: [OperationsProductController], providers: [OperationsProductRepository, OperationsRbacService, OperationsProductService], exports: [OperationsProductService, OperationsRbacService] })
+@Module({ imports: [IdentityModule, AnalyticsModule], controllers: [OperationsProductController], providers: [OperationsProductRepository, OperationsRbacService, OperationsProductService], exports: [OperationsProductService, OperationsRbacService] })
 export class OperationsProductModule {}
