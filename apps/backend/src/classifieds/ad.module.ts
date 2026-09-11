36182bea5fd8aaacad3a074d394bfaafa72feab5
khedma-sy <khedmasy@gmail.com>
2026-09-11 01:09:05 +0000

    fix: enforce Classifieds schema on enabled startup

--- apps/backend/src/classifieds/ad.module.ts
+++ apps/backend/src/classifieds/ad.module.ts
@@ -6,12 +6,13 @@ import { AdminAdController, AdController } from './ad.controller';
 import { AdminAdMediaController, AdMediaController } from './ad-media.controller';
 import { AdMediaService } from './ad-media.service';
 import { AdRepository } from './ad.repository';
+import { AdSchemaGuard } from './ad-schema.guard';
 import { AdService } from './ad.service';
 
 @Module({
   imports: [DatabaseModule, IdentityModule, OperationsProductModule],
   controllers: [AdController, AdminAdController, AdMediaController, AdminAdMediaController],
-  providers: [AdRepository, AdService, AdMediaService],
+  providers: [AdRepository, AdService, AdMediaService, AdSchemaGuard],
   exports: [AdService]
 })
 export class AdModule {}
