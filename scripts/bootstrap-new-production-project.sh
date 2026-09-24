0760669f17ae9eb593597ef924e5b7ea27ae802f
khedma-sy <khedmasy@gmail.com>
2026-09-24 23:32:55 +0300

    fix: bind production bootstrap to immutable GitHub OIDC identity

--- scripts/bootstrap-new-production-project.sh
+++ scripts/bootstrap-new-production-project.sh
@@ -13,6 +13,9 @@ if [[ "${GOOGLE_CLOUD_REGION:-}" != "europe-west1" ]]; then
 fi
 
 CANONICAL_GITHUB_REPOSITORY="khedma-sy/khedmah-digital-v1"
+CANONICAL_GITHUB_REPOSITORY_ID="1307435925"
+CANONICAL_GITHUB_REPOSITORY_OWNER_ID="307214577"
+CANONICAL_GITHUB_ENVIRONMENT="production"
 GITHUB_REPOSITORY="${GITHUB_REPOSITORY:-$CANONICAL_GITHUB_REPOSITORY}"
 TF_STATE_BUCKET="${TF_STATE_BUCKET:-${GOOGLE_CLOUD_PROJECT}-khedmah-tfstate}"
 TF_STATE_LOCATION="${TF_STATE_LOCATION:-$GOOGLE_CLOUD_REGION}"
@@ -276,6 +279,9 @@ terraform_vars=(
   "-var=build_service_account_id=khedmah-v1-build"
   "-var=migration_service_account_id=khedmah-v1-migrator"
   "-var=github_repository=$GITHUB_REPOSITORY"
+  "-var=github_repository_id=$CANONICAL_GITHUB_REPOSITORY_ID"
+  "-var=github_repository_owner_id=$CANONICAL_GITHUB_REPOSITORY_OWNER_ID"
+  "-var=github_environment=$CANONICAL_GITHUB_ENVIRONMENT"
   "-var=github_workflow_path=.github/workflows/production-operator-new-account.yml"
   '-var=github_additional_workflow_paths=[".github/workflows/production-operator.yml",".github/workflows/production-baseline-001-020.yml",".github/workflows/production-bootstrap-admin.yml",".github/workflows/production-migrations-025-034.yml",".github/workflows/production-database-role-bootstrap.yml",".github/workflows/terraform-media-apply.yml",".github/workflows/terraform-media-plan.yml",".github/workflows/terraform-media-state-handoff.yml",".github/workflows/terraform-client-maps-plan.yml",".github/workflows/terraform-client-maps-apply.yml",".github/workflows/android-release-certification.yml"]'
   '-var=github_ref=refs/heads/main'
@@ -284,7 +290,7 @@ terraform_vars=(
 
 verify_plan_target() {
   local plan_json="$1"
-  jq -e     --arg project "$GOOGLE_CLOUD_PROJECT"     --arg region "$GOOGLE_CLOUD_REGION"     --arg state_bucket "$TF_STATE_BUCKET"     --arg repository "$CANONICAL_GITHUB_REPOSITORY"     --arg source_commit "$CURRENT_SHA"     --arg configuration_sha256 "$CONFIGURATION_SHA256" '
+  jq -e     --arg project "$GOOGLE_CLOUD_PROJECT"     --arg region "$GOOGLE_CLOUD_REGION"     --arg state_bucket "$TF_STATE_BUCKET"     --arg repository "$CANONICAL_GITHUB_REPOSITORY"     --arg repository_id "$CANONICAL_GITHUB_REPOSITORY_ID"     --arg repository_owner_id "$CANONICAL_GITHUB_REPOSITORY_OWNER_ID"     --arg github_environment "$CANONICAL_GITHUB_ENVIRONMENT"     --arg source_commit "$CURRENT_SHA"     --arg configuration_sha256 "$CONFIGURATION_SHA256" '
       .complete == true and
       .errored == false and
       .variables.project_id.value == $project and
@@ -293,6 +299,9 @@ verify_plan_target() {
       .variables.configuration_sha256.value == $configuration_sha256 and
       .variables.terraform_state_bucket_name.value == $state_bucket and
       .variables.github_repository.value == $repository and
+      .variables.github_repository_id.value == $repository_id and
+      .variables.github_repository_owner_id.value == $repository_owner_id and
+      .variables.github_environment.value == $github_environment and
       .variables.github_ref.value == "refs/heads/main" and
       .variables.github_workflow_path.value == ".github/workflows/production-operator-new-account.yml" and
       .variables.artifact_registry_repository_id.value == "khedmah-digital" and
